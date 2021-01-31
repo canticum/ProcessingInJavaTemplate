0e63c6ac832196170a71892948be87fc406fb746
Jonathan Chang, Chun-yien <ccy@musicapoetica.org>
2021-02-01 06:25:57 +0800

    update dependency & remove system scaling.

--- src/main/java/MySketch.java
+++ src/main/java/MySketch.java
@@ -1,32 +1,34 @@
+
 import processing.core.*;
 
 public class MySketch extends PApplet {
 
-    PImage p3Img;
+  PImage p3Img;
 
-    @Override
-    public void settings() {
-        
-        size(300, 300);
+  @Override
+  public void settings() {
+
+    size(300, 300);
+  }
+
+  @Override
+  public void setup() {
+
+    p3Img = loadImage(getClass().getResource("nctu.png").getFile());
+  }
+
+  @Override
+  public void draw() {
+
+    background(255);
+    if (mousePressed) {
+      image(p3Img, (width - p3Img.pixelWidth) / 2, (height - p3Img.height) / 2);
     }
+  }
 
-    @Override
-    public void setup() {
-        
-        p3Img = loadImage(getClass().getResource("nctu.png").getFile());
-    }
+  public static void main(String[] args) {
 
-    @Override
-    public void draw() {
-        
-        background(255);
-        if (mousePressed) {
-            image(p3Img, (width - p3Img.pixelWidth) / 2, (height - p3Img.height) / 2);
-        }
-    }
-
-    public static void main(String[] args) {
-
-        PApplet.main("MySketch");
-    }
+    System.setProperty("sun.java2d.uiScale", "1.0");
+    PApplet.main("MySketch");
+  }
 }
